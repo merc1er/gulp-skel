b58c56e428e53f9a170e6c64eaae9f9183ec1db1
Corentin Mercier <corentin@mercier.link>
2020-10-05 00:37:07 +0700

    Add JavaScript concat/minify and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,34 @@
-let gulp = require('gulp'),
-    sass = require('gulp-sass'),
+// Imports
+
+const gulp = require('gulp'),
     del = require('del'),
     newer = require('gulp-newer'),
-    browserSync = require('browser-sync'),
+    // CSS processing
+    sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
+    // Templating
     nunjucksRender = require('gulp-nunjucks-render'),
+    // JavaScript handling
+    concat = require('gulp-concat'),
+    terser = require('gulp-terser'),
+    // Development browser
+    browserSync = require('browser-sync'),
     reload = browserSync.reload;
 
 
 // TASKS
+
+// Clean previous build
+
 gulp.task('clean', function(done){
   // Deletes all files from dist/
   del.sync('dist/', {force: true});
   done()
 });
 
+
 // Nunjucks
+
 gulp.task('nunjucks', function() {
   // Gets all .html files in pages
   return gulp.src('app/**/*.html')
@@ -27,6 +40,9 @@ gulp.task('nunjucks', function() {
   .pipe(gulp.dest('dist'))
 });
 
+
+// Sass preprocessing and CSS minifying
+
 gulp.task('sass', function(){
   return gulp.src('scss/style.scss')
     .pipe(sass()) // Compiles styles.scss to css
@@ -35,12 +51,31 @@ gulp.task('sass', function(){
     .pipe(reload({
       stream: true
     }))
+  });
+
+
+// Concat and minify JavaScript
+
+gulp.task('js', function() {
+  const files = [
+    // Uncomment 2 lines below to add Bootstrap JS support
+    // 'node_modules/jquery/dist/jquery.slim.min.js',
+    // 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    'js/*.js'
+  ];
+  return gulp.src(files)
+    .pipe(concat('scripts.js'))
+    // Minify JS
+    .pipe(terser())
+    .pipe(gulp.dest('dist/static/js/'));
 });
 
+
 // Copy all static files
+
 gulp.task('copy-static', function(){
   return gulp.src('app/static/**/*.*', {base: './app/static/'})
-    .pipe(gulp.dest('dist/static/'));
+  .pipe(gulp.dest('dist/static/'));
 });
 
 gulp.task('reload', function(done){
@@ -48,16 +83,23 @@ gulp.task('reload', function(done){
   done();
 });
 
+
 // Watch for changes
+
 gulp.task('watch', function(done){
   // Watch HTML pages
-  gulp.watch('app/**/*.html', gulp.series('nunjucks', 'copy-static', 'reload'));
+  gulp.watch('app/**/*.html',
+             gulp.series('nunjucks', 'copy-static', 'reload'));
   // Watch SCSS files
   gulp.watch('scss/**/*.scss', gulp.series('sass', 'copy-static'));
+  // Watch JS files
+  gulp.watch('js/*.js', gulp.series('js', 'reload'));
   done();
 });
 
+
 // Starts browserSync
+
 gulp.task('serve', function(done){
   browserSync({
     server: {
@@ -73,9 +115,9 @@ gulp.task('serve', function(done){
 
 
 // Default task
-gulp.task('default', gulp.series('clean', 'sass', 'nunjucks',
+gulp.task('default', gulp.series('clean', 'sass', 'nunjucks', 'js',
   'copy-static', 'serve', 'watch'));
 
 // Deployment task
-gulp.task('build', gulp.series('clean', 'sass', 'nunjucks',
+gulp.task('build', gulp.series('clean', 'sass', 'nunjucks', 'js',
   'copy-static'));
